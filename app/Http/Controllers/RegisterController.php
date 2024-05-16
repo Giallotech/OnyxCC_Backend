c1817fb350fa9db21f76282c6d68e2f5d130acf6
Giallotech <70095038+Giallotech@users.noreply.github.com>
2024-05-16 19:43:42 +0200

    Minor changes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -10,12 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller {
   public function __invoke(Request $request) {
-    // $request->validate([
-    //   'username' => ['required', 'string', 'min:2', 'max:255', Rule::unique(User::class)],
-    //   'email' => ['required', 'string', 'email', 'max:255', Rule::unique(User::class)],
-    //   'password' => ['required', 'string']
-    // ]);
-
     try {
       $request->validate([
         'name' => ['required', 'string', 'max:255'],
@@ -24,12 +18,6 @@ class RegisterController extends Controller {
         'password' => ['required', 'string'],
         'registration_code' => 'required',
       ]);
-
-      // $user = User::create([
-      //   'username' => $request->get('username'),
-      //   'email' => $request->get('email'),
-      //   'password' => Hash::make($request->get('password'))
-      // ]);
 
       $role = 'user';
       if ($request->registration_code === env('ADMIN_REGISTRATION_CODE')) {
@@ -44,8 +32,8 @@ class RegisterController extends Controller {
         'role' => $role,
       ]);
 
-      // Auth::login($user);
-      // $request->session()->regenerate();
+      Auth::login($user);
+      $request->session()->regenerate();
       // By calling the Auth::login() method, we are logging the user in after registration. We are also regenerating the session to prevent session fixation attacks.
 
       return response()->json(
