26c493214e75dd00aec95a8e844fa96d5f046039
Giallotech <70095038+Giallotech@users.noreply.github.com>
2024-08-03 22:29:57 +0200

    refactor: Remove unused files

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\File;
 
-// Route::get('/', function () {
-//   return view('welcome');
-// });
-
-// This route is for serving the static files from the React app, such as the index.html file, the CSS and JS files, and the images.
-Route::get('/{any?}', function ($any = null) {
-  $path = public_path('dist/' . $any);
-
-  if (File::isDirectory($path)) {
-    // If the path is a directory, serve the index.html file
-    return File::get(public_path('dist/index.html'));
-  }
-
-  if (File::exists($path)) {
-    // If the file exists, serve it
-    return File::get($path);
-  }
-
-  // If the file doesn't exist, serve the index.html file
-  return File::get(public_path('dist/index.html'));
-})->where('any', '.*');
+Route::get('/', function () {
+  return view('welcome');
+});
